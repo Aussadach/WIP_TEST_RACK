d37442f0a2214afef38a26e54f555c7501f91469
Aussadach <aussadachmasun@gmail.com>
2021-06-11 21:38:54 +0700

    "Add Model"

---
+++ Model/Model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+
+
+def join_table():
+
+    pass
+
+def check_duplicated():
+    
+    
+    pass
+
+
+def load_rack_db():
+
+
+    pass
+
+def unload_rack_db():
+
+
+    pass
+
+
+def get_all_table():
+
+
+
+    pass
+
+def export_excel():
+
+
+
+    pass
+
+
+def upload_excel_pickle():
+    
+
+    pass
